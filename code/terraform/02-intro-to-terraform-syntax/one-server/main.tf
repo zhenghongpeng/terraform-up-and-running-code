3cd90924db77abd46b91ff5db88d27623922b95a
zpeng38 <zpeng38@bitbucket.org>
2020-10-19 14:53:20 -0500

    init

--- code/terraform/02-intro-to-terraform-syntax/one-server/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-server/main.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.12, <= 0.13.4"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 
   # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami           = "ami-2757f631"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "terraform-example"
